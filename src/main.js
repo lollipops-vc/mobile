864e78862cec15af6d305bc112688adc589295ea
lollipops-vc <898902463@qq.com>
2022-11-18 18:43:39 +0800

    创建文件夹

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
+
+// 导入vue
 import Vue from 'vue'
+// 导入App
 import App from './App.vue'
+// 导入路由
 import router from './router'
+// 导入vuex
 import store from './store'
 
 Vue.config.productionTip = false
 
+// 创建了vue实例
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+  router, // 挂载了路由
+  store, // 挂载了vuex
+  render: h => h(App)// 挂载App.vue
+}).$mount('#app')// 挂载到页面上的id名为app的容器中
